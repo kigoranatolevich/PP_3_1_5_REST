9385f97fbaa2d5e387ac45934d5fa88865696871
Igor Krivoruchko <kigoranatolevich@gmail.com>
2023-05-27 13:56:18 +0300

    Create util package. Rename TestClass to DataLoader

--- src/main/java/ru/kata/spring/boot_security/demo/util/DataLoader.java
+++ src/main/java/ru/kata/spring/boot_security/demo/util/DataLoader.java
@@ -1,4 +1,4 @@
-package ru.kata.spring.boot_security.demo;
+package ru.kata.spring.boot_security.demo.util;
 
 import org.springframework.stereotype.Component;
 import ru.kata.spring.boot_security.demo.entity.Role;
@@ -9,12 +9,12 @@ import javax.annotation.PostConstruct;
 import java.util.Set;
 
 @Component
-public class TestClass {
+public class DataLoader {
     private final User user;
     private final UserService userService;
     private final RoleRepository roleRepository;
 
-    public TestClass(User user, UserService userService, RoleRepository roleRepository) {
+    public DataLoader(User user, UserService userService, RoleRepository roleRepository) {
         this.user = user;
         this.userService = userService;
         this.roleRepository = roleRepository;
